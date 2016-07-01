ce73213e4237ec571c609d68e03b7126971a0266
Edmilson Vasconcelos Jr <ejunior@gmail.com>
2016-07-01 16:38:56 -0300

    Add files via upload
    mathplotlib implementado

--- consumer-producer_model.py
+++ consumer-producer_model.py
@@ -1,8 +1,8 @@
-import functools
+from functools import reduce
 import math
 import random
 import csv
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 
 __author__ = 'ur5f'
@@ -59,15 +59,14 @@ class Consumer:
 
                 if self.demands > cheapest_supply:
                     self.demands -= cheapest_supply
-                    cheapest_producer.supply = 0
+                    Market.cheapest_producer().supply = 0
                 else:
-                    cheapest_producer.supply -= self.demands
+                    Market.cheapest_producer().supply -= self.demands
                     self.demands = 0
 
 
 producers = []
-for i in range(NUM_OF_PRODUCERS):
-    
+for _z in range(NUM_OF_PRODUCERS):
     producer = Producer()
     producer.price = COST + random.choice(range(MAX_STARTING_PROFIT))  # rand(MAX_STARTING_PROFIT)
     producer.supply = random.choice(range(MAX_STARTING_SUPPLY))  # rand(MAX_STARTING_SUPPLY)
@@ -76,7 +75,7 @@ for i in range(NUM_OF_PRODUCERS):
 
 consumers = []
 # NUM_OF_CONSUMERS.times do
-for i in range(NUM_OF_CONSUMERS):
+for _i in range(NUM_OF_CONSUMERS):
     consumers.append(Consumer())
 
 generated_demand = []
@@ -84,22 +83,29 @@ generated_demand = []
 for n in range(SIMULATION_DURATION):
     generated_demand.append(round((math.sin(n)+2)*20))
 
+# reading demand data
+# with open('foo.csv', newline='') as csvfile:
+#    line = csv.reader(csvfile, delimiter=',')
+#     for r in line:
+#         generated_demand.append(int(r[1]))
+#     print(generated_demand)
+
 
 class Market:
     @staticmethod
     def average_price():
         # ($producers.inject(0.0) { |memo, producer| memo + producer.price}/ $producers.size).round(2)
-        return round(functools.reduce(lambda x, y: x+y.price, producers, 0)/producers.__len__(), 2)
+        return round(reduce(lambda x, y: x+y.price, producers, 0)/producers.__len__(), 2)
 
     @staticmethod
     def supply():
         # $producers.inject(0) { |memo, producer| memo + producer.supply }
-        return functools.reduce(lambda x, y: x+y.supply, producers, 0)
+        return reduce(lambda acc, y: acc+y.supply, producers, 0)
 
     @staticmethod
     def demand():
         # $consumers.inject(0) { |memo, consumer| memo + consumer.demands }
-        return functools.reduce(lambda x, y: x+y.demands, consumers, 0)
+        return reduce(lambda x, y: x+y.demands, consumers, 0)
 
     @staticmethod
     def cheapest_producer():
@@ -117,53 +123,80 @@ class Market:
 
 
 demand_supply = []
-price =[]
-supply = []
-demand = []
 price_demand = []
 
+price = []
+supply = []
+demand = []
+
+
 # SIMULATION_DURATION.times do |t|
+# for t in range(SIMULATION_DURATION):
 for t in range(SIMULATION_DURATION):
-    print(t)
+
+    progress = t/SIMULATION_DURATION
+    print("\rProgress: [{0:50s}] {1:.1f}%".format('#' * int(progress * 50), progress*100), end="", flush=True)
+
     # consumers.each do |consumer|
     for consumer in consumers:
         consumer.demands = generated_demand[t]
 
     demand_supply.append([t, Market.demand(), Market.supply()])
-    supply.append( Market.supply())
+    supply.append(Market.supply())
     # demand_supply << [t, Market.demand, Market.supply]
     # $producers.each do |producer|
     for producer in producers:
         producer.produce()
 
-    price_demand.append([t, Market.average_price(), Market.demand()])
     price.append(Market.average_price())
     demand.append(Market.demand())
+    price_demand.append([t, Market.average_price(), Market.demand()])
 
     while Market.demand() > 0 and Market.supply() > 0:
         # $consumers.each do |consumer|
         for consumer in consumers:
             consumer.buy()
 
-
-fig ,ax1 = plt.subplots()
+# price demand
+fig, ax1 = plt.subplots(nrows=1, ncols=2)
 t = range(SIMULATION_DURATION)
 
-ax1.plot(t, price, 'b-' )
-ax1.set_xlabel('time (s)')
+ax1[0].plot(t, price, 'b-')
+ax1[0].set_xlabel('time (months)')
 # Make the y-axis label and tick labels match the line color.
-ax1.set_ylabel('price', color='b')
-for tl in ax1.get_yticklabels():
+ax1[0].set_ylabel('price', color='b')
+ax1[0].yaxis.grid(True)
+ax1[0].xaxis.grid(True)
+
+for tl in ax1[0].get_yticklabels():
     tl.set_color('b')
 
-ax2 = ax1.twinx()
+ax2 = ax1[0].twinx()
 ax2.plot(t, demand, 'r-')
-ax2.set_ylabel('demamd', color='r')
+ax2.set_ylabel('demand', color='r')
+ax2.yaxis.grid(True)
+
 for tl in ax2.get_yticklabels():
     tl.set_color('r')
+
+# demand supply
+ax1[1].plot(t, demand, 'b-')
+ax1[1].set_xlabel('time (months)')
+
+ax1[1].set_ylabel('demand', color='b')
+ax1[1].yaxis.grid(True)
+for tl in ax1[1].get_yticklabels():
+    tl.set_color('b')
+
+ax3 = ax1[1].twinx()
+ax3.plot(t, supply, 'r-')
+ax3.set_ylabel('supply', color='r')
+ax3.yaxis.grid(True)
+
+for tl in ax3.get_yticklabels():
+    tl.set_color('r')
+
 plt.show()
-
-
 
 
 outputFile = open('price_demand.csv', 'w', newline='')
